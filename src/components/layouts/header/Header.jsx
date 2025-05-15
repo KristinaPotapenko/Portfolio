af023ff26bf16d618c562733846ae84d5179bdcb
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-15 19:54:31 +0300

    Header style improvement

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -2,17 +2,18 @@ import { Link } from "../../ui/link/Link";
 import { Navigation } from "../../ui/navigation/Navigation";
 import { ReactComponent as DownloadIcon } from "../../../assets/icons/download.svg";
 import { Logo } from "../../ui/logo/Logo";
+import style from "./Header.module.scss";
 
 export const Header = () => {
   return (
-    <header>
-      <div className="container">
+    <div className="container">
+      <header className={style.header}>
         <Logo />
         <Navigation />
         <Link href="/Resume.png" download>
           Resume <DownloadIcon />
         </Link>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
