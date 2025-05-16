f18b97e3cc07a15b6406fb0ad2ce3c2ad091c3a1
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-16 13:26:49 +0300

    Update hero heading styles

--- src/components/hero/HeroHeading.jsx
+++ src/components/hero/HeroHeading.jsx
@@ -1,17 +1,17 @@
-import style from "./HeroHeading.module.scss";
+import style from "../ui/sectionTitle/Heading.module.scss";
 
 export const HeroHeading = () => {
   return (
-    <h1 className={style.heroTitle}>
+    <h1 className={style.title}>
       Hello I’am
-      <span className={style.heroTitleStrong}> Kristina</span>
-      <span className={style.heroTitleStrong}> Frontend </span>
-      <span className={style.heroTitleDecoration}>
-        <span className={style.heroTitleDecorationStroke}>Developer</span>
-        <span className={style.heroTitleDecorationFill}>Developer</span>
+      <span className={style.titleStrong}> Kristina</span>
+      <span className={style.titleStrong}> Frontend </span>
+      <span className={style.titleDecoration}>
+        <span className={style.titleDecorationStroke}>Developer</span>
+        <span className={style.titleDecorationFill}>Developer</span>
       </span>
       Based In
-      <span className={style.heroTitleStrong}> Ukraine</span>
+      <span className={style.titleStrong}> Ukraine</span>
     </h1>
   );
 };
