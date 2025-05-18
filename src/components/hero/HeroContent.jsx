5e284b64524d284f4f0c1096db0f0d031b2f47ca
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-18 14:52:56 +0300

    change margin and fill in description

--- src/components/hero/HeroContent.jsx
+++ src/components/hero/HeroContent.jsx
@@ -6,11 +6,16 @@ export const HeroContent = () => {
   return (
     <div>
       <HeroHeading />
-      <SectionDescription marginBottom="88px">
-        I'm Flora Sheen Lorem Ipsum is simply dummy text of the printing and
-        typesetting industry. Lorem Ipsum has been the industry's standard dummy
-        text ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it to specimen book.
+      <SectionDescription marginBottom="60px">
+        I have taught myself web development and am highly motivated to continue
+        developing in this area. I have experience creating user-friendly and
+        attractive interfaces for my own projects, as well as experience working
+        in a team on one of my projects. I started my career in design and
+        worked with mockups in Figma, but I realized over time that development
+        brought me real pleasure. I love developing new projects, honing my
+        skills, and designing user-centered solutions. I am looking for an
+        opportunity to join a team where I can grow as a developer and
+        contribute to a common goal.
       </SectionDescription>
       <SocialBlock />
     </div>
