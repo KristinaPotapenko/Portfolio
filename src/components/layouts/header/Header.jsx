b06b245371cc579a15506f8942d0185c0362c725
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-20 11:03:57 +0300

    Create mobile menu, adaptive and refactoring performed

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -2,17 +2,42 @@ import { Link } from "../../ui/link/Link";
 import { Navigation } from "../../ui/navigation/Navigation";
 import { ReactComponent as DownloadIcon } from "../../../assets/icons/download.svg";
 import { Logo } from "../../ui/logo/Logo";
+import { ReactComponent as BurgerIcon } from "../../../assets/icons/burger.svg";
+import { ReactComponent as CloseIcon } from "../../../assets/icons/close.svg";
 import style from "./Header.module.scss";
+import { useState } from "react";
+import { useNoScroll } from "../../../scripts/hooks/useNoScroll";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  useNoScroll(isOpen);
+
+  const handleChangeOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="container">
       <header className={style.header}>
         <Logo />
-        <Navigation />
-        <Link href="/Resume.png" download>
-          Resume <DownloadIcon />
-        </Link>
+        <div className={`${style.headerLinks} ${isOpen && style.isActive}`}>
+          <Navigation onClick={handleChangeOpen} />
+          <Link onClick={handleChangeOpen} href="/Resume.png" download>
+            Resume <DownloadIcon />
+          </Link>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className={style.closeButton}
+          >
+            <CloseIcon />
+          </button>
+        </div>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className={style.headerBurger}
+        >
+          <BurgerIcon />
+        </button>
       </header>
     </div>
   );
