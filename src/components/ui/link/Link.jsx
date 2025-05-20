478420f60bbb19c2639c184ed0099d9aac49842b
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-20 11:33:46 +0300

    Add click event

--- src/components/ui/link/Link.jsx
+++ src/components/ui/link/Link.jsx
@@ -1,8 +1,8 @@
 import style from "../../inputs/button/Button.module.scss";
 
-export const Link = ({ children, ...props }) => {
+export const Link = ({ onClick, children, ...props }) => {
   return (
-    <a className={style.button} {...props}>
+    <a className={style.button} onClick={() => onClick()} {...props}>
       {children}
     </a>
   );
