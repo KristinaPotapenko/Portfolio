2f46d08fdf28a3bb5e61598602147849f144841c
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-20 11:34:45 +0300

    Add props

--- src/components/ui/navigation/Navigation.jsx
+++ src/components/ui/navigation/Navigation.jsx
@@ -2,20 +2,20 @@ import { NavLink } from "../navLink/NavLink";
 
 import style from "./Navigation.module.scss";
 
-export const Navigation = () => {
+export const Navigation = ({ ...props }) => {
   return (
     <nav>
       <ul className={style.navigationList}>
-        <NavLink to="about" smooth="easeInOutCubic">
+        <NavLink {...props} to="about" smooth="easeInOutCubic">
           About Me
         </NavLink>
-        <NavLink to="skills" smooth="easeInOutCubic">
+        <NavLink {...props} to="skills" smooth="easeInOutCubic">
           Skills
         </NavLink>
-        <NavLink to="projects" smooth="easeInOutCubic">
+        <NavLink {...props} to="projects" smooth="easeInOutCubic">
           Project
         </NavLink>
-        <NavLink to="contact" smooth="easeInOutCubic">
+        <NavLink {...props} to="contact" smooth="easeInOutCubic">
           Contact me
         </NavLink>
       </ul>
