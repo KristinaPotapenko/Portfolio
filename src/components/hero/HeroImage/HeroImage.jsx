a28f37e4c18a5e966ed888128652d55ca1ae0b15
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-05-20 16:31:43 +0300

    Update image alt

--- src/components/hero/HeroImage/HeroImage.jsx
+++ src/components/hero/HeroImage/HeroImage.jsx
@@ -2,5 +2,5 @@ import PersonImage from "../../../assets/images/hero.jpg";
 import style from "./HeroImage.module.scss";
 
 export const HeroImage = () => {
-  return <img className={style.image} src={PersonImage} alt="Person photo" />;
+  return <img className={style.image} src={PersonImage} alt="Person" />;
 };
