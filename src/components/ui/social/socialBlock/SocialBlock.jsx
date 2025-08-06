74fad6593500951ecb923d6006ea991e06c3ffbb
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-06 21:38:58 +0300

    feat: add animation for SocialBlock component

--- src/components/ui/social/socialBlock/SocialBlock.jsx
+++ src/components/ui/social/socialBlock/SocialBlock.jsx
@@ -1,13 +1,45 @@
+import { useEffect, useRef } from "react";
+
+import gsap from "gsap";
+
 import { SocialItem } from "../socialItem/SocialItem";
+
 import { ReactComponent as InstagramIcon } from "../../../../assets/icons/instagram.svg";
 import { ReactComponent as TelegramIcon } from "../../../../assets/icons/telegram.svg";
 import { ReactComponent as GmailIcon } from "../../../../assets/icons/gmail.svg";
 import { ReactComponent as GithubIcon } from "../../../../assets/icons/github.svg";
+
 import style from "./SocialBlock.module.scss";
 
 export const SocialBlock = () => {
+  const socialBlockRef = useRef(null);
+
+  useEffect(() => {
+    if (!socialBlockRef.current) return;
+
+    const items = socialBlockRef.current.querySelectorAll("li");
+
+    gsap.fromTo(
+      items,
+      {
+        y: 30,
+        opacity: 0,
+        scale: 0.9,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        stagger: 0.2,
+        duration: 0.8,
+        delay: 0.4,
+        ease: "back.out(1.7)",
+      }
+    );
+  }, {});
+
   return (
-    <ul className={style.socialBlock}>
+    <ul ref={socialBlockRef} className={style.socialBlock}>
       <SocialItem href="https://www.instagram.com/_k.vladimirovnaa_?igsh=MTBrY3huNDAydzFjYQ%3D%3D&utm_source=qr">
         <InstagramIcon />
       </SocialItem>
