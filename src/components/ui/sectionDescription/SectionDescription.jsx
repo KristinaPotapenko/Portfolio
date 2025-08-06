4d38ef2ada26aab0456497231e2c0079cc2962a7
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-06 13:11:11 +0300

    feat: add animation for SectionDescription component

--- src/components/ui/sectionDescription/SectionDescription.jsx
+++ src/components/ui/sectionDescription/SectionDescription.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useRef } from "react";
 import style from "./SectionDescription.module.scss";
+import gsap from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
 
 export const SectionDescription = ({ marginBottom, children }) => {
+  const descriptionRef = useRef(null);
+
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      gsap.set(descriptionRef.current, { opacity: 1 });
+
+      let split;
+      SplitText.create(descriptionRef.current, {
+        type: "words,lines",
+        linesClass: "line",
+        autoSplit: true,
+        mask: "lines",
+        onSplit: (self) => {
+          split = gsap.from(self.lines, {
+            yPercent: 100,
+            opacity: 0,
+            stagger: 0.2,
+            duration: 1.5,
+            delay: 0.2,
+            ease: "expo.out",
+          });
+          return split;
+        },
+      });
+    });
+  }, []);
+
   return (
-    <p style={{ marginBottom }} className={style.sectionDescription}>
+    <p
+      ref={descriptionRef}
+      style={{ marginBottom }}
+      className={style.sectionDescription}
+    >
       {children}
     </p>
   );
