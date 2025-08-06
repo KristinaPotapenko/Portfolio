8b32f5ff6148bf1aff8ea4157292bbab1aca0d67
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-06 12:45:33 +0300

    feat: add animation for HeroHeading component

--- src/components/hero/HeroContent/HeroHeading.jsx
+++ src/components/hero/HeroContent/HeroHeading.jsx
@@ -1,17 +1,46 @@
+import { useEffect, useRef } from "react";
+
+import gsap from "gsap";
+
 import style from "../../ui/sectionTitle/Heading.module.scss";
 
 export const HeroHeading = () => {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    if (!titleRef.current) return;
+
+    const words = titleRef.current.querySelectorAll("span");
+
+    gsap.fromTo(
+      words,
+      {
+        opacity: 0,
+        y: 20,
+        scale: 0.95,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scale: 1,
+        stagger: 0.2,
+        duration: 0.8,
+        delay: 0.4,
+        ease: "back.out(1.7)",
+      }
+    );
+  }, []);
+
   return (
-    <h1 className={style.title}>
-      Hello I’am
-      <span className={style.titleStrong}> Kristina Frontend</span>
-      <span className={style.titleStrong}> </span>{" "}
+    <h1 ref={titleRef} className={style.title}>
+      <span>Hello</span> <span>I’am</span>{" "}
+      <span className={style.titleStrong}>Kristina</span>{" "}
+      <span className={style.titleStrong}>Frontend</span>{" "}
       <span className={style.titleDecoration}>
         <span className={style.titleDecorationStroke}>Developer</span>
         <span className={style.titleDecorationFill}>Developer</span>
       </span>{" "}
-      Based In
-      <span className={style.titleStrong}> Ukraine</span>
+      <span>Based In</span> <span className={style.titleStrong}>Ukraine</span>
     </h1>
   );
 };
