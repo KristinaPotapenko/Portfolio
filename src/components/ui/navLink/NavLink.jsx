6943d672b30fd394b4d1c3fd9ddfbb47bd665846
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-08-07 11:52:09 +0300

    refactor: move NavLink hover animation from SCSS to GSAP

--- src/components/ui/navLink/NavLink.jsx
+++ src/components/ui/navLink/NavLink.jsx
@@ -1,9 +1,43 @@
+import { useRef } from "react";
 import { Link } from "react-scroll";
+
+import gsap from "gsap";
+
 import style from "./NavLink.module.scss";
 
 export const NavLink = ({ onClick, children, ...props }) => {
+  const linkRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (!linkRef.current) return;
+
+    gsap.to(linkRef.current, {
+      y: -4,
+      scale: 1.05,
+      boxShadow: "0px 10px 25px rgba(0,0,0,0.08)",
+      duration: 0.3,
+      ease: "power3.out",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    if (!linkRef.current) return;
+
+    gsap.to(linkRef.current, {
+      y: 0,
+      scale: 1,
+      boxShadow: "0px 0px 0px rgba(0,0,0,0)",
+      duration: 0.3,
+      ease: "power3.out",
+    });
+  };
+
   return (
-    <li>
+    <li
+      ref={linkRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <Link className={style.navLink} onClick={() => onClick()} {...props}>
         {children}
       </Link>
